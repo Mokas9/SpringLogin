bae84efd0971e58007af03f5e1a01e3d6dc357ae
Maxim Grushevsky <m.grushevsky10@gmail.com>
2017-09-07 16:42:13 +0300

    New pages
    Added pages for application

---
+++ target/SpringLogin/WEB-INF/classes/database.sql
@@ -0,0 +1,30 @@
+-- Table: users
+CREATE TABLE users(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  username VARCHAR(255) NOT NULL ,
+  password VARCHAR(255) NOT NULL
+)ENGINE = InnoDB;
+
+-- Table: roles
+CREATE TABLE roles(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  name VARCHAR(100) NOT NULL
+)ENGINE = InnoDB;
+
+-- Table for mapping users and roles: user_roles
+CREATE TABLE user_roles(
+  user_id INT NOT NULL ,
+  role_id INT NOT NULL ,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+
+  UNIQUE (user_id, role_id)
+)ENGINE = InnoDB;
+
+INSERT INTO users VALUE (1, 'proselyte', '12345');
+
+INSERT INTO roles VALUE (1, 'ROLE_USER');
+INSERT INTO roles VALUE (2, 'ROLE_ADMIN');
+
+INSERT INTO user_roles VALUE (1, 2);
